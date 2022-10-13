d198c64885569527ab143165bd0ad32c55a95b31
sunnyfur <polin.makarova@mail.ru>
2022-10-13 17:32:29 +0500

    feat: start recursive  cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,35 +1,50 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
- * 
+ *
  * @example
- * 
+ *
  * const directMachine = new VigenereCipheringMachine();
- * 
+ *
  * const reverseMachine = new VigenereCipheringMachine(false);
- * 
+ *
  * directMachine.encrypt('attack at dawn!', 'alphonse') => 'AEIHQX SX DLLU!'
- * 
+ *
  * directMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => 'ATTACK AT DAWN!'
- * 
+ *
  * reverseMachine.encrypt('attack at dawn!', 'alphonse') => '!ULLD XS XQHIEA'
- * 
+ *
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
- * 
+ *
  */
 class VigenereCipheringMachine {
+  static alp = "abcdefghijklmnopqrstuvwxyz";
   encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (arguments.length != 2 || !arguments[0] || !arguments[1])
+      throw new Error("Incorrect arguments!");
+    const text = arguments[0];
+    const word = arguments[1];
+    let res = "";
+    const wLeng = word.length;
+    let currWordIndex = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (currWordIndex < wLeng) {
+        currWordIndex++;
+      }
+      currWordIndex = 0;
+    }
+    return res;
   }
   decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (arguments.length != 2 || !arguments[0] || !arguments[1])
+      throw new Error("Incorrect arguments!");
+    const text = arguments[0];
+    const word = arguments[1];
   }
 }
 
 module.exports = {
-  VigenereCipheringMachine
+  VigenereCipheringMachine,
 };
