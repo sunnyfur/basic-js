f23d787f9a126f4ca640e37b31295617b630b7a4
Polina <sunnyfur9@gmail.com>
2022-10-15 12:42:15 +0500

    feat: cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,28 +20,59 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-  static alp = "abcdefghijklmnopqrstuvwxyz";
+  alp = "abcdefghijklmnopqrstuvwxyz";
+
   encrypt() {
     if (arguments.length != 2 || !arguments[0] || !arguments[1])
       throw new Error("Incorrect arguments!");
-    const text = arguments[0];
-    const word = arguments[1];
+    const text = arguments[0].toLowerCase();
+    const word = arguments[1].toLowerCase();
     let res = "";
     const wLeng = word.length;
     let currWordIndex = 0;
     for (let i = 0; i < text.length; i++) {
-      if (currWordIndex < wLeng) {
-        currWordIndex++;
+      if (currWordIndex == wLeng) {
+        currWordIndex = 0;
       }
-      currWordIndex = 0;
+      this.alp.indexOf(text[i]) < 0 || text[i] === " "
+        ? (res += text[i])
+        : (res +=
+            this.alp[
+              (this.alp.indexOf(text[i]) +
+                this.alp.indexOf(word[currWordIndex++])) %
+                this.alp.length
+            ]);
     }
-    return res;
+    return res.toUpperCase();
   }
   decrypt() {
     if (arguments.length != 2 || !arguments[0] || !arguments[1])
       throw new Error("Incorrect arguments!");
-    const text = arguments[0];
-    const word = arguments[1];
+    const text = arguments[0].toLowerCase();
+    const word = arguments[1].toLowerCase();
+    let res = "";
+    const wLeng = word.length;
+    let currWordIndex = 0;
+    for (let i = 0; i < text.length; i++) {
+      if (currWordIndex == wLeng) {
+        currWordIndex = 0;
+      }
+      if (this.alp.indexOf(text[i]) < 0 || text[i] === " ") res += text[i];
+      else {
+        res +=
+          this.alp[
+            (this.alp.indexOf(text[i]) -
+              this.alp.indexOf(word[currWordIndex]) >=
+            0
+              ? this.alp.indexOf(text[i]) -
+                this.alp.indexOf(word[currWordIndex++])
+              : this.alp.indexOf(text[i]) -
+                this.alp.indexOf(word[currWordIndex++]) +
+                this.alp.length) % this.alp.length
+          ];
+      }
+    }
+    return res.toUpperCase();
   }
 }
 
